1d5f4cbe644a3b6751ec4f9338c8921e5ddd724d
Jean-Baptiste Note <jb.note@criteo.com>
2015-05-12 10:11:54 +0200

    Generate configuration with generic template

--- koya-slider-package/package/scripts/params.py
+++ koya-slider-package/package/scripts/params.py
@@ -9,8 +9,12 @@ app_user = config['configurations']['global']['app_user']
 pid_file = config['configurations']['global']['pid_file']
 app_log_dir = config['configurations']['global']['app_log_dir']
 kafka_version = config['configurations']['global']['kafka_version']
+xmx = config['configurations']['global']['xmx_val']
+xms = config['configurations']['global']['xms_val']
 
 conf_dir = format("{app_root}/config")
-brokerConfig = config['configurations']['broker'].copy()
-brokerConfig['broker.id'] = int(config['configurations']['global']['app_container_tag']) - 1
 
+broker_config=dict(line.strip().split('=') for line in open(format("{conf_dir}/server.properties")) if not (line.startswith('#') or re.match(r'^\s*$', line)))
+broker_config.update(config['configurations']['broker'])
+broker_prop=broker_config['content']
+del broker_config['content']
